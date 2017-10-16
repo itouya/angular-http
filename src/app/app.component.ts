c4836ce5c2cbda3e2616e83f14e2824daa114e64
伊藤靖彦 <yasuhiko@chelonia.local>
2017-10-16 23:44:30 +0900

    imlements HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+class Hero {
+  id: number;
+  name: string;
+  account_image: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  results: Hero[];
+
+  constructor(private http: HttpClient ) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:8080/api/hero') .subscribe((data: Hero[]) => {
+      console.log(data);
+      this.results = data;
+    });
+  }
 }
